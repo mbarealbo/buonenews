a4daf95788c889b656a5d5e37298c3764f2f3139
Francesco Pasqua <cesconix@me.com>
2024-10-03 17:01:23 +0200

    chore: minor changes

--- src/utils/common/get-sentiment-by-domain.ts
+++ src/utils/common/get-sentiment-by-domain.ts
@@ -2,8 +2,8 @@ import type { Sentiment } from "../openai/types";
 
 const isProd = import.meta.env.PROD;
 
-export const getSentimentByDomain = (domain: string): Sentiment | null => {
-  let sentiment: Sentiment | null = null;
+export const getSentimentByDomain = (domain: string): Sentiment => {
+  let sentiment: Sentiment = "positive";
 
   if (isProd) {
     sentiment = domain.includes("buone") ? "positive" : "negative";
--- src/utils/datocms/content-delivery-api/fetch-evaluated-articles.ts
+++ src/utils/datocms/content-delivery-api/fetch-evaluated-articles.ts
@@ -6,11 +6,12 @@ import type { Sentiment } from "@/utils/openai/types";
 import { createCDAClient } from "./create-cda-client";
 
 export const fetchEvaluatedArticles = async (
-  sentiment: Sentiment | null,
+  sentiment: Sentiment,
 ): Promise<EvaluatedArticleFragment[]> => {
   const client = createCDAClient();
-  const filter = sentiment ? { sentiment } : {};
-  const query = await client.request(AllEvaluatedArticlesDocument, filter);
+  const query = await client.request(AllEvaluatedArticlesDocument, {
+    sentiment,
+  });
 
   return query.allEvaluatedArticles;
 };
